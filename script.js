6610c91a6d0c98146f2d2188579483fbeb97aa34
juan-cp <juanoale@gmail.com>
2022-07-05 09:18:28 -0400

    boton para borrar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var listado = document.getElementById("listado");
 var btnSave = document.getElementById("btnSave");
 var listado_ingresos;
+var borrar_icono;
 var ultimo_id = 0;
 
 function inicializar() {
@@ -13,8 +14,7 @@ function inicializar() {
     }
     btnSave.addEventListener('click', guarda_paciente);
 
-  }
-  
+}
 function guarda_paciente(event) {
 
     var paciente = {
@@ -28,33 +28,32 @@ function guarda_paciente(event) {
     listado_ingresos.push(paciente);
     sincronizar();
     mostrar_pacientes(paciente);
+    actualizabotones(); 
     ultimo_id++;
-  }
-
-  function sincronizar() {
+}
+function sincronizar() {
   
     window.localStorage.setItem('listado_ingresos', JSON.stringify(listado_ingresos));
     listado_ingresos = JSON.parse(window.localStorage.getItem('listado_ingresos'));
-  }
-
-  function obtener_ultimo_id() {
+}
+function obtener_ultimo_id() {
     var ultimo_paciente = listado_ingresos[listado_ingresos.length - 1];
     ultimo_id = ultimo_paciente.pacienteId + 1;
     return ultimo_id;
-  }
-  function mostrar_pacientes(pac) {
+}
+function mostrar_pacientes(pac) {
   
-    var removeIcon = document.createElement('td');
+    var borrar_icono = document.createElement('td');
     var element = document.createElement('tr');
     var updateIcon = document.createElement('td');
 
   
-    removeIcon.innerHTML = "&#10060;";
-    removeIcon.className = "remove_item clickeable";
-    removeIcon.setAttribute("title", "Remove");
+    borrar_icono.innerHTML = "&#10060;";
+    borrar_icono.className = "borra clickeable";
+    borrar_icono.setAttribute("title", "Remove");
 
     updateIcon.innerHTML = "&#9997;";
-    updateIcon.className = "update_icon clickeable";
+    updateIcon.className = "actualiza clickeable";
     updateIcon.setAttribute("title", "Update");
 
 
@@ -64,9 +63,32 @@ function guarda_paciente(event) {
     element.innerHTML += "<td>"+pac.apellido+"</td>";
     element.innerHTML += "<td>"+pac.email_add+"</td>";
     element.innerHTML += "<td>"+pac.telefono+"</td>";
-    element.appendChild(removeIcon);
+    element.appendChild(borrar_icono);
     element.appendChild(updateIcon);
     listado.appendChild(element);
+}
+function borrar_pac(event) {
+  
+  var BORRAR_pac= event.currentTarget.parentNode;
+  var pacienteId = BORRAR_pac.id;
+  listado.removeChild(BORRAR_pac);
+  listado_ingresos.forEach(function(value, i) {
+    if (value.pacienteId == paciente_id) {
+      listado_ingresos.splice(i, 1);
+    }
+  })
+
+  sincronizar();
+}
+function actualizabotones() {
+  
+  borrar_icono = document.getElementsByClassName("borra");
+  if (!!borrar_icono.length) {
+    for (var i = 0; i < borrar_icono.length; i++) {
+      borrar_icono[i].addEventListener('click', borrar_pac);
+    }
+  }
+  
 }
 
 inicializar();
